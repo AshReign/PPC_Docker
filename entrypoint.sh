b8ffe496166ab3c2de28032b79153664544620a2
AshReign <51983826+AshReign@users.noreply.github.com>
2025-05-29 14:42:17 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 analyze_vowels.py > results.txt
 git config --global --add safe.directory /github/workspace
+python3 analyze_vowels.py > results.txt
 USERNAME=$(git log -1 --pretty=format:'%an')
 ./update_readme.sh "$USERNAME"
